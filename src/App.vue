19e13964458f45bbeec568fadf122df63d171786
Faboo <fabienleroy2004@gmail.com>
2024-01-25 10:59:07 +0100

    feat: add message input

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
-<script setup></script>
+<script setup>
+</script>
 
-<template>
-    <div class="bg-gradient-to-r from-blue-600 to-black ">NavBar</div>
-    <RouterView></RouterView>
+<template >
+    <div class="bg-black h-full">
+        <div class="bg-gradient-to-r from-blue-600 to-black">NavBar</div>
+        <RouterView></RouterView>
+    </div>
 </template>
 
 <style scoped></style>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,6 +1,42 @@
-<template >
-    <div class="bg-gradient-to-r from-red-600 to-black ">
-        Salut monde!
+<script setup>
+import { ref } from 'vue'
+const message = ref('')
+const messages = ref([])
+const addMessage = () => {
+  if (message.value != '') {
+    messages.value.push(message.value)
+    message.value = ''
+  }
+}
+</script>
+<template>
+  <div>
+    <div v-for="(message, index) in messages" :key="index">
+      <h1
+        class="text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl dark:text-white text-white bg-gradient-to-r from-lime-400 to-black p-4"
+      >
+        {{ message }}
+      </h1>
     </div>
-    
+  </div>
+
+  <div class="bg-gradient-to-r from-red-600 to-black">
+    <div class="flex p-5">
+      <textarea
+        v-model="message"
+        name="message"
+        id="message"
+        cols="30"
+        rows="10"
+        class="bg-black text-neutral-50 border-white border m-2"
+      ></textarea>
+      <button
+        @click="addMessage"
+        type="button"
+        class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+      >
+        Envoyer
+      </button>
+    </div>
+  </div>
 </template>
