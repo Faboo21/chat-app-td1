1872a5e57cacbdb64dfa4f6a20bebf5f57f2a433
Faboo <fabienleroy2004@gmail.com>
2024-02-21 09:12:46 +0100

    fin jour 2

---
+++ src/components/AppInput.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import { defineProps } from 'vue';
+    
+    const props = defineProps({
+        modelValue: String
+    })
+    const emit = defineEmits(['update:modelValue'])
+
+    const value = computed();
+</script>
+
+<template>
+    <input v-bind="$attrs" class="mt-2 mb-2 bg-black border-2 border-white rounded-md" required>
+</template>
---
+++ src/views/SignupView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AppInput from '@/components/AppInput.vue';
+import { reactive } from 'vue';
+
+const {formValues} = reactive({
+    email: '',
+    username: '',
+    password: '',
+})
+</script>
+
+<template>
+    <div class="">
+        <form action="" class="flex flex-col gap-2 p-4 mx-auto text-white max-w-96">
+            <h1 class="mb-2 text-xl">Creer un compte :</h1>
+            <label for="email">Email : </label>
+            <AppInput :modelValue="formValues.email" type="email" id="email" name="email" autocomplete="email"/>
+            <label for="username">Username : </label>
+            <AppInput :modelValue="formValues.username" type="text" name="username" id="username" autocomplete="username"/>
+            <label for="password">Mot de passe : </label>
+            <AppInput :modelValue="formValues.password" type="password" name="password" id="password" autocomplete="new-password"/>
+            <button type="submit" class="mt-5 text-black bg-white rounded-md">Creer un compte</button>
+        </form>
+    </div>
+</template>
